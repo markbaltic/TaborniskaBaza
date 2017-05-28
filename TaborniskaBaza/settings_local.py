960f4dad960f7bee7c5bf9a29f9c1948c3fd4d80
julijatominc <julija.tominc@yahoo.com>
2017-05-28 09:24:46 +0200

    Posodobi settings_local.

--- TaborniskaBaza/settings_local.py
+++ TaborniskaBaza/settings_local.py
@@ -5,10 +5,10 @@ from TaborniskaBaza.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'TBaza',
-        'USER': 'tabornik',
-        'PASSWORD': 'tabornik',
-        'HOST': 'localhost',
+        'NAME': 'sem2017_julijat',
+        'USER': 'julijat',
+        'PASSWORD': 'v206huao',
+        'HOST': 'baza.fmf.uni-lj.si',
         'PORT': '5432',
     }
 }
