44a639338bd1a998f0af33a629cf86017491d61d
julijatominc <julija.tominc@yahoo.com>
2018-01-05 16:41:29 +0100

    Dodaj vod v search_results. Spremeni views.

--- taborniki/urls.py
+++ taborniki/urls.py
@@ -7,11 +7,13 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 
-    url(r'^index', views.index, name='index'),
+
     url(r'^search', views.search_results, name='search'),
     url(r'^clani', views.clani, name='clani'),
     url(r'^profil/(?P<clan_id>[0-9]+)$', views.get_name , name='profil'),
     url(r'^dodaj', views.dodajClan , name='dodaj'),
+    url(r'^vod/(?P<vod_id>[0-9]+)$', views.get_vod , name='vod'),
+    url(r'^[a-zA-Z]', views.index, name='index'),
 
     url(r'^', auth_views.login, {'template_name': 'taborniki/login.html'} , name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
--- taborniki/views.py
+++ taborniki/views.py
@@ -5,7 +5,7 @@ from itertools import chain
 from django.shortcuts import render, redirect
 import django.contrib.postgres.search as search
 from django.http import HttpResponse
-from taborniki.models import Oseba
+from taborniki.models import Oseba, Vod
 from django.db.models import Q
 from .forms import NameForm, DodajClan, Search
 
@@ -14,15 +14,25 @@ def search_results(request):
     form = Search(request.GET)
     if form.is_valid():
         isci = form.cleaned_data['q']
+        # poiščemo po imenu
+        clani1 = Oseba.objects.filter(ime__contains=isci)
+        # poiščemo po priimku
+        clani2 = Oseba.objects.filter(priimek__contains=isci)
+        # združimo in odstranimo duplikate
+        clani = list(set(list(chain(clani1, clani2))))
+        # najdemo vode
+        vodi = list(Vod.objects.filter(imeVod__contains = isci))
+        if clani:
+            return render(request, 'taborniki/tabele.html', {'clani': clani, 'vodi':vodi})
+        else:
+            return render(request, 'taborniki/no_results.html', {'isci': isci})
     else:
         redirect('/taborniki/index')
-    # poiščemo po imenu
-    clani1 = Oseba.objects.filter(ime__contains=isci)
-    # poiščemo po priimku
-    clani2 = Oseba.objects.filter(priimek__contains=isci)
-    # združimo in odstranimo duplikate
-    clani = list(set(list(chain(clani1, clani2))))
-    return render(request, 'taborniki/tabele.html', {'clani':clani})
+
+
+
+
+
 
 
 def index(request):
@@ -44,7 +54,9 @@ def get_name(request, clan_id):
     clan = Oseba.objects.get(id = clan_id)
     return render(request, 'taborniki/search_results.html', {'clan': clan})
 
-
+def get_vod(request, vod_id):
+    vod = Vod.objects.get(id = vod_id)
+    return render(request, 'taborniki/vod.html', {'Vod': vod})
 
 def dodajClan(request):
     if request.method == 'POST':
